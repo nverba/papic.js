92e8de470f704b9118da772065ea2abd1fab6ca1
Ted Wen <ted@makeitsocial.com>
2015-07-30 14:56:48 +0100

    disable local server test

--- test/testServer.js
+++ test/testServer.js
@@ -3,20 +3,18 @@ var express = require('express');
 var app = express();
 
 var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 var papi_client = require('../lib/main.js');
 app.use('/papi', express.static(papi_client.staticPath));
 papi_client.route(app);
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-
-// app.post('/',function(req,res){
-//     console.log(':::: headers:',req.headers);
-//     console.log(':::: req.body:',JSON.stringify(req.body,null,4));
-//     res.send({status:'OK'});
-// });
+app.post('/',function(req,res){
+    console.log(':::: headers:',req.headers);
+    console.log(':::: req.body:',JSON.stringify(req.body,null,4));
+    res.send({status:'OK'});
+});
 
 var server = app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function () {
     var addr = server.address();
