af20f4b8df91590fa3278184dfbe969426e57e04
Ted Wen <ted@makeitsocial.com>
2015-07-30 14:23:56 +0100

    added validate function

--- test/testServer.js
+++ test/testServer.js
@@ -1,41 +1,22 @@
+/* global process */
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded());
+
 var papi_client = require('../lib/main.js');
 app.use('/papi', express.static(papi_client.staticPath));
+papi_client.route(app);
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
+// app.post('/',function(req,res){
+//     console.log(':::: headers:',req.headers);
+//     console.log(':::: req.body:',JSON.stringify(req.body,null,4));
+//     res.send({status:'OK'});
 // });
-
-// endpoint to test pl frontend
-// app.get('/scrapi', function(req, res,next){
-//     var n = req.url.indexOf('?'), ps = req.url.substr(n);
-//     console.log('\n---- router.get. url=', req.url, ', ps=', ps);
-//     if (ps == '/') ps = '';
-//     res.redirect('/popup.html'+ps);
-// });
-
-// scrapi.init({
-//     papi: {
-//         url: process.env.MIS_PAPI_HOST || 'localhost:8080',
-//         username: process.env.PAPI_CLIENTID || 'makeitso',
-//         password: process.env.PAPI_SECRET || '123456',
-//         hashkey: 'makeitso'
-//     },
-//     gapi: {
-//         url: process.env.MIS_GAPI_HOST || 'localhost:8000',
-//         username: process.env.GAPI_CLIENTID || 'makeitso',
-//         password: process.env.GAPI_SECRET || '123456'
-//     },
-// });
-// scrapi.listen(server);
-// scrapi.route(app);
 
 var server = app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function () {
     var addr = server.address();
